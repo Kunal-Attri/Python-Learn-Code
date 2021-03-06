46c97ca28bbcf964c82b35d54b77e9a4f3e6d615
Kunal Attri <you@example.com>
2021-03-06 10:34:12 +0530

    modified

--- 018 - Installing Python On Linux.py
+++ 018 - Installing Python On Linux.py
@@ -1,5 +1,16 @@
 # Installing Python on Linux
 
+# Go to python.org
+# Go to downloads
+# Download the latest Source Code file
+# Open terminal and type the following commands one by one
+# cd Downloads
+# tar -xf Python-... (filename)
+# cd Pyhton-... (filename)
+# .configure
+# sudo make altinstall
+# sudo make install
+# You are done!!!
 
 
 # Thank You !!!
