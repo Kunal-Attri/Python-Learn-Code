44f04321bf563639b46a4f2669bdf33a57b19313
Kunal Attri <attrikunal16@gmail.com>
2021-03-08 22:07:33 +0530

    multi line strings code added

---
+++ 033 - Triple Quotes.py
@@ -0,0 +1,17 @@
+# Multi-line strings using Triple Quotes
+
+# You can write multi line strings or paragraphs by
+# enclosing the whole in triple quotes
+
+a = """Hi there,
+how are you?"""
+
+b = '''Hello,
+I am good.
+What about you?'''
+
+print(a)
+print(b)
+
+
+# Thank You !!!
