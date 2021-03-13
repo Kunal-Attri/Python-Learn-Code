f674aa004b3daa8ccffd914baae1267064f8f980
Kunal Attri <attrikunal16@gmail.com>
2021-03-13 19:43:53 +0530

    Installing matplotlib added

---
+++ 038 - Installing Matplotlib.py
@@ -0,0 +1,18 @@
+# Installing matplotlib
+
+# To install matplotlib, first open cmd or terminal, but before
+# that confirm that you don't have matplotlib already
+
+import matplotlib
+
+# this should give error
+
+
+# So, first open cmd or terminal and pip install it
+# And it has been installed. No errors.
+# In next few videos , we will be playing a lot with matplotlib,
+# by plotting various types of graphs, objects, animations, etc
+
+
+
+# Thank You !!!
