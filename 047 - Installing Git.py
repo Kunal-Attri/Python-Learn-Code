7bea0db579636710d74c73c38a1ffab83ce4b11f
Kunal <attrikunal16@gmail.com>
2021-05-19 05:26:55 -0700

    Installing Git added

---
+++ 047 - Installing Git.py
@@ -0,0 +1,9 @@
+# Installing Git on Windows
+
+# Step 1 - Open Browser, go to https://www.git-scm.com/downloads/
+# Step 2 - Click on your OS type (Windows in our case)
+# Step 3 - Let the file download, and then open it
+# Step 4 - Click Next, Next, Next... Install (No need to change anything)
+# Step 5 - You are done and ready to use Git!
+
+# Thank You!!!
